c762f964dfea09ee67077b00bba71dfda04bb1b8
Adrian Kern <adrianmilokern@gmail.com>
2021-06-23 16:36:23 -0700

    updated es vertex shader

--- resources/shaders/es-shaders/basicVert.glsl
+++ resources/shaders/es-shaders/basicVert.glsl
@@ -3,16 +3,18 @@ layout (location = 0) in vec3 vPos;
 layout (location = 1) in vec2 vTexCoords;
 layout (location = 2) in vec3 vColor;
 layout (location = 3) in vec3 vNormal;
+layout (location = 4) in mat4 model;
 
 out highp vec2 passTextureCoords;
 out highp vec3 passNormal;
 out highp vec3 fragPos;
 out highp vec3 passColor;
 
-uniform mat4 model;
 uniform mat4 projection;
 uniform mat4 view;
+
 uniform highp int mode;
+
 //modes:
 //0: Texture, with projection and view matrix
 //1: Color, with projection and view matrix
@@ -24,10 +26,11 @@ void main(){
         gl_Position = model * vec4(vPos, 1.0);
     } else {
         gl_Position = projection * view * model * vec4(vPos, 1.0);
+        passNormal = mat3(transpose(inverse(model))) * vNormal;
+        fragPos = vec3(model * vec4(vPos, 1.0));
     }
+
     passTextureCoords = vTexCoords;
-    passNormal = mat3(transpose(inverse(model))) * vNormal;
-    fragPos = vec3(model * vec4(vPos, 1.0));
     passColor = vColor;
 }
 
