595a3908b427366cb293399a0153f56d80e8d707
Adrian Kern <adrianmilokern@gmail.com>
2021-07-19 19:23:43 -0700

    changed texture

--- src/cubes/Window.java
+++ src/cubes/Window.java
@@ -147,7 +147,7 @@ public class Window implements Runnable {
                 skyBox = new TextureRenderer(new TexturedMesh(Geometry.CUBE_VERTICES, Geometry.CUBE_TEX_COORDS, Geometry.CUBE_NORMALS, "textures/skybox.png", 0));
                 player = new TextureRenderer("models/iron_man/IronMan/IronMan.obj", "models/iron_man/iron_man_tex.jpg", 1);
                 //player = new TextureRenderer("resources/models/rocket_ship/rocket.obj", "textures/asteroid.png", 0);
-                planets = new TextureRendererMulti("models/xbox/xbox.obj", "models/xbox/xbox_tex.png", 0, planetPositions, planetScales, planetRots);
+                planets = new TextureRendererMulti("models/xbox/xbox.obj", "models/xbox/xbox_tex.jpg", 1, planetPositions, planetScales, planetRots);
                 //asteroids = new TextureRendererMulti(new TexturedMesh(Geometry.PYRAMID_VERTICES, Geometry.PYRAMID_TEX_COORDS, Geometry.PYRAMID_NORMALS, "textures/asteroid.png"), asteroidPositions, asteroidScales, asteroidRots);
                 asteroids = new TextureRendererMulti("models/backpack/backpack.obj", "models/backpack/diffuse.jpg", 1, asteroidPositions, asteroidScales, asteroidRots);
                 light = new TextureRenderer("models/donut/Donut.obj", "models/donut/Tekstur_donat.png", 1);
